953fab696014bad54a3ddcaf2d511f83dbec5dbf
vimal2535 <vimalaroan5@gmail.com>
2024-07-29 11:53:57 +0530

    Add files via upload

---
+++ Hangman/troop.py
@@ -0,0 +1,127 @@
+word_list = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Strawberry",
+    "Blueberry",
+    "Raspberry",
+    "Blackberry",
+    "Watermelon",
+    "Kiwi",
+    "Papaya",
+    "Guava",
+    "Grapes",
+    "Cherry",
+    "Peach",
+    "Plum",
+    "Pear",
+    "Pomegranate",
+    "Fig",
+    "Date",
+    "Dragon fruit",
+    "Passion fruit",
+    "Lychee",
+    "Durian",
+    "Coconut",
+    "Avocado",
+    "Carrot",
+    "Broccoli",
+    "Cauliflower",
+    "Spinach",
+    "Tomato",
+    "Cucumber",
+    "Onion",
+    "Garlic",
+    "Potato",
+    "Sweet potato",
+    "Pumpkin",
+    "Eggplant",
+    "Mushroom",
+    "Peas",
+    "Green beans",
+    "Corn",
+    "Radish",
+    "Cabbage"
+]
+
+level=[
+"""
+ _______
+   |    |
+   0    |
+        |
+        |
+        |
+        |
+      -----
+""",
+
+"""
+________
+  |     |
+  0     |
+  |     |
+        |
+        |
+        |
+      -----
+""",
+
+"""
+________
+   |    |
+   0    |
+   |-   |
+        |
+        |
+        |
+      -----
+""",
+
+"""
+ ________
+    |    |
+    0    | 
+   -|-   |
+         |
+         |
+         |
+       -----
+""",
+
+"""
+________
+   |    |
+   0    |
+  -|-   |
+    \   |
+        |
+        |
+      -----
+""",
+
+"""
+________
+   |    |
+   0    |
+  -|-   |
+  / \   |
+        |
+        |
+      -----
+"""
+]
+
+logo="""
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/
+"""
+
