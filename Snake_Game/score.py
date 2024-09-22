76307c452d4683659d753d96b0b69ba6e1ed9bf5
vimal2535 <vimalaroan5@gmail.com>
2024-09-22 10:46:42 +0530

    Update score.py

--- Snake_Game/score.py
+++ Snake_Game/score.py
@@ -1,19 +1,34 @@
 from turtle import Turtle
+from time import sleep
 FONT= ("Arial",15,"normal")
 ALIGN = "center"
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.scor=0
+        with open("high_score.txt", mode="r") as fill:
+            self.high_score = int(fill.read())
+        self.clear()
         self.penup()
         self.hideturtle()
         self.goto(0, 290)
-        self.write("SCORE: " + str(self.scor), False, align=ALIGN,font= FONT)
+        self.write(f"SCORE:{self.scor} HIGH SCORE:{self.high_score}", False, align=ALIGN,font=FONT)
     def point(self):
         self.clear()
         self.scor += 1
-        self.write("SCORE: " + str(self.scor), False, align=ALIGN,font=FONT)
+        self.goto(0, 290)
+        self.write(f"SCORE:{self.scor} HIGH SCORE:{self.high_score}", False, align=ALIGN, font=FONT)
+
     def end(self):
-        self.color("red")
-        self.goto(0,0)
-        self.write("GAME OVER!",move=False,align=ALIGN,font=FONT)
+        self.clear()
+        with open("high_score.txt", "r") as file:
+            self.high_score = int(file.read())
+        if self.scor > int(self.high_score):
+            with open("high_score.txt","w") as new:
+                new.write(str(self.scor))
+            with open("high_score.txt") as final:
+                self.high_score = int(final.read())
+        self.scor=0
+        self.color("black")
+        self.goto(0,290)
+        self.write(f"SCORE:{self.scor} HIGH SCORE:{self.high_score}", False, align=ALIGN, font=FONT)
